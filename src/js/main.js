24276ed0f6dea25e74985394c1a0099e52f30d8d
Krakovsky <work.krakovsky@gmail.com>
2020-10-27 00:17:16 +0200

    add skills section

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,40 @@ import '../scss/main.scss';
 
 // Libs
 import tilt from 'tilt.js';
+import ActiveMenuLink from 'active-menu-link';
 
 $(() => {
+  const header = $('.header');
+  let justExecuted = false;
+
+  if (window.scrollY >= 90) {
+    header.addClass('scrolled')
+  }
+
+  new ActiveMenuLink('.header__nav', {
+    itemTag: 'li',
+    activeClass: 'active',
+    ease: 'linear',
+    headerHeight: 120,
+  });
+
+  window.addEventListener('scroll', () => {
+    if (justExecuted) {
+      return;
+    }
+
+    justExecuted = true;
+
+    if (window.scrollY >= 90) {
+      header.addClass('scrolled')
+    } else {
+      header.removeClass('scrolled')
+    }
+
+    setTimeout(function () {
+      justExecuted = false;
+    }, 50);
+  });
+
   $('.js-tilt').tilt();
 })
